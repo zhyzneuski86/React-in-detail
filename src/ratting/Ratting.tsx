ffefda378e6f5bd809aa4fbf7145f7dd3e6da666
zhyzneuski86 <1293728@gmail.com>
2023-03-12 17:33:20 +0300

    Rendering

---
+++ src/ratting/Ratting.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {Simulate} from "react-dom/test-utils";
+import select = Simulate.select;
+type RattingPropsType = {
+    value: number
+}
+export function Ratting(props: RattingPropsType) {
+    console.log("Ratting rendering")
+    return (
+        <div>
+            <Star selected={props.value > 0}/>
+            <Star selected={props.value > 1}/>
+            <Star selected={props.value > 2}/>
+            <Star selected={props.value > 3}/>
+            <Star selected={props.value > 4}/>
+
+        </div>
+
+    );
+}
+type StarPropsType = {
+    selected: boolean
+}
+
+function Star(props: StarPropsType) {
+    console.log("Star rendering")
+    if (props.selected === true) {
+        return <span><b>star </b></span>
+    } else {
+        return <span>star </span>
+    }
+
+}
