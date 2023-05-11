dd38aca8b91e54c3294e0902778cac71dec28cc5
zhyzneuski86 <1293728@gmail.com>
2023-05-11 21:16:28 +0300

    add array of items to Accordion

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -10,11 +10,22 @@ const meta: Meta<typeof Accordion> = {
 
 export default meta;
 const callback = action('accordion mode change event fired')
-export const MenuCollapsedMode = ()=> < Accordion titleValue={'Menu'} collapsed={true} onChange={callback} />
-export const UsersUncollapsedMode = ()=> <Accordion titleValue={'Users'} collapsed={false} onChange={callback} />
+const onClickCallback = action('some item was clicked')
+
+export const MenuCollapsedMode = ()=> < Accordion titleValue={'Menu'} collapsed={true} onChange={callback} items={[]}
+onClick={onClickCallback}/>
+export const UsersUncollapsedMode = ()=> <Accordion titleValue={'Users'} collapsed={false} onChange={callback}
+         items={[{title:'Dima', value: 1}, {title:'Mikita', value: 2},  {title:'Artem', value:3},  {title:'Misha', value:3}]}
+onClick={onClickCallback}/>
 
 
 export const  ModeChange = ()=> {
     const [value, setValue]= useState<boolean>(true)
-    return < Accordion titleValue={'Users'} collapsed={value} onChange={()=>setValue(!value)}/>
+    return < Accordion titleValue={'Users'} collapsed={value} onChange={()=>setValue(!value)}
+           items={[
+               {title:'Dima', value: 1},
+               {title:'Mikita', value: 2},
+               {title:'Artem', value:3},
+               {title:'Misha', value:3}]}
+                       onClick={(value)=>{alert(`user with ID ${value} should be happy`)}}/>
 }
