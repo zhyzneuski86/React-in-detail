bac7b522bdb8119f80c676ce2a8ac9ce946da7d5
zhyzneuski86 <1293728@gmail.com>
2023-07-03 21:53:55 +0300

    change UseEffect.stories.tsx

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -32,3 +32,31 @@ export const Example1 = () => {
 
     </>
 }
+
+export const SetTimeoutExample = () => {
+
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log('SetTimeoutExample')
+
+
+    useEffect(()=>{
+        console.log('useEffect first render and every counter change')
+
+        setTimeout(()=>{
+            console.log('setTimeout')
+            document.title = counter.toString()
+        }, 1000)
+
+    }, [counter])
+
+
+
+    return <>
+        Hollow, {counter} {fake}
+        <button onClick={() => {setFake(fake + 1)}}>+ fake</button>
+        <button onClick={() => {setCounter(counter + 1)}}>+ counter</button>
+
+    </>
+}
